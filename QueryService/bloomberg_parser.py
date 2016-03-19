068f78ba60145d3c390057cb22bdfe6f0cd83124
hungtantran <hungtantran@live.com>
2016-03-19 04:22:36 -0700

    Basic bloomberg table parser

--- QueryService/bloomberg_parser.py
+++ QueryService/bloomberg_parser.py
@@ -16,21 +16,35 @@ class BloombergParser:
         link_handle = urllib.urlopen(link)
         soup = BeautifulSoup(link_handle.read(), self.parser_type)
 
-        data_rows = soup.findAll('tr', attrs={'class': 'data-table__row'})
-        for row in data_rows:
-            name = row.findAll('td', attrs={'class': 'data-table__row__cell', 'data-type': 'name'})
-            if len(name) != 1:
-                logger.Logger.log(logger.LogLevel.WARN, 'There are %d names for row %s' % (len(name), name))
-                continue
-            else:
-                print(name)
+        table_containers = soup.findAll('div', attrs={'class': 'table-container'})
 
-            value = row.findAll('td', attrs={'class': 'data-table__row__cell', 'data-type': 'value'})
-            if len(value) != 1:
-                logger.Logger.log(logger.LogLevel.WARN, 'There are %d values for row %s' % (len(value), value))
+        # Iterate through each table
+        for table in table_containers:
+            # Find the header and parse it
+            header = table.findAll('tr', attrs={'class': 'data-table__headers'})
+            if len(header) != 1:
+                logger.Logger.log(logger.LogLevel.WARN, 'Found %d headers, more than 1' % (len(header)))
                 continue
-            else:
-                print(value)
+
+            header_cells = header[0].findAll('th', attrs={'class': 'data-table__headers__cell'})
+            # A list of titles of the table like ['INDEX', 'UNITS', 'PRICE', 'CHANGE', '%CHANGE']
+            header_cell_titles = [cell.get_text() for cell in header_cells]
+
+            # Parse all the data rows from the current table
+            data_rows = soup.findAll('tr', attrs={'class': 'data-table__row'})
+            for row in data_rows:
+                row_cells = row.findAll('td', attrs={'class': 'data-table__row__cell'})
+                if (len(row_cells) != len(header_cell_titles)):
+                    logger.Logger.log(
+                            logger.LogLevel.WARN,
+                            'Found %d value cells not matching with %d title cells' % (
+                                    len(row_cells), len(header_cell_titles)))
+                    continue
+
+                # A list of values like ['CL1:COM', 'USD/bll.', '39.44', '-1.89%']
+                row_cell_values = [cell.get_text() for cell in row_cells]
+                titles_and_values = zip(header_cell_titles, row_cell_values)
+                logger.Logger.log(logger.LogLevel.INFO, 'Found following values in the cell: %s' % titles_and_values)
 
 
 if __name__ == '__main__':
