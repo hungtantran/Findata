397534ebf5b11081af3e03cc8f5ae00d2bce863d
hungtantran <hungtantran@live.com>
2016-03-19 04:35:00 -0700

    Add clean_name

--- QueryService/bloomberg_parser.py
+++ QueryService/bloomberg_parser.py
@@ -5,11 +5,18 @@ from bs4 import BeautifulSoup
 import urllib
 
 import logger
-
+import re
 
 class BloombergParser:
     def __init__(self):
         self.parser_type = 'html.parser'
+
+    @staticmethod
+    def clean_name(name_str):
+        name_str = name_str.strip()
+        name_str = name_str.replace('\n', '')
+        name_str = re.sub('( )+', ' ', name_str)
+        return name_str
 
     def parse(self, link):
         logger.Logger.log(logger.LogLevel.INFO, 'BloombergParser attempts to parse link \'%s\'' % (link))
@@ -42,11 +49,12 @@ class BloombergParser:
                     continue
 
                 # A list of values like ['CL1:COM', 'USD/bll.', '39.44', '-1.89%']
-                row_cell_values = [cell.get_text() for cell in row_cells]
+                row_cell_values = [BloombergParser.clean_name(cell.get_text()) for cell in row_cells]
+                # Zip the titles and values together to create a list of tuple (title, value)
                 titles_and_values = zip(header_cell_titles, row_cell_values)
                 logger.Logger.log(logger.LogLevel.INFO, 'Found following values in the cell: %s' % titles_and_values)
 
 
 if __name__ == '__main__':
     bloomberg = BloombergParser();
-    bloomberg.parse('http://www.bloomberg.com/markets/commodities')
+    bloomberg.parse('http://www.bloomberg.com/energy')
