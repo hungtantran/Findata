07ed53d30c2c0038ecf1b9fdc0ab550ee8db20f0
hungtantran <hungtantran@live.com>
2016-03-19 21:53:54 -0700

    Add more bloomberg parser test

--- QueryService/bloomberg_parser.py
+++ QueryService/bloomberg_parser.py
@@ -3,6 +3,7 @@ __author__ = 'hungtantran'
 
 from bs4 import BeautifulSoup
 import urllib
+import unicodedata
 
 import logger
 import re
@@ -16,35 +17,54 @@ class BloombergParser:
         name_str = name_str.strip()
         name_str = name_str.replace('\n', '')
         name_str = re.sub('( )+', ' ', name_str)
+        name_str = unicodedata.normalize('NFKD', name_str).encode('ascii','ignore')
+        name_str = name_str.lower()
         return name_str
 
     @staticmethod
-    def parse_table_containers(html_elem):
+    def parse_table_containers_from_html(html_elem):
         return html_elem.findAll('div', attrs={'class': 'table-container'})
+
+    @staticmethod
+    def parse_header_titles_from_table(table_html_elem):
+        header = table_html_elem.findAll('tr', attrs={'class': 'data-table__headers'})
+        if len(header) != 1:
+            logger.Logger.log(logger.LogLevel.WARN, 'Found %d headers, more than 1' % (len(header)))
+            return None
+
+        header_cells = header[0].findAll('th', attrs={'class': 'data-table__headers__cell'})
+        # A list of titles of the table like ['INDEX', 'UNITS', 'PRICE', 'CHANGE', '%CHANGE']
+        header_cell_titles = [BloombergParser.clean_name(cell.get_text()) for cell in header_cells]
+        return header_cell_titles
+
+    @staticmethod
+    def parse_rows_from_table(table_html_elem):
+        return table_html_elem.findAll('tr', attrs={'class': 'data-table__row'})
+
+    @staticmethod
+    def parse_values_from_row(row_html_elem):
+        row_cells = row_html_elem.findAll('td', attrs={'class': 'data-table__row__cell'})
+        # A list of values like ['CL1:COM', 'USD/bll.', '39.44', '-1.89%']
+        row_cell_values = [BloombergParser.clean_name(cell.get_text()) for cell in row_cells]
+        return row_cell_values
 
     def parse(self, link):
         logger.Logger.log(logger.LogLevel.INFO, 'BloombergParser attempts to parse link \'%s\'' % (link))
         link_handle = urllib.urlopen(link)
         html = BeautifulSoup(link_handle.read(), self.parser_type)
-
-        table_containers = self.parse_table_containers(html)
+        table_containers = BloombergParser.parse_table_containers_from_html(html)
 
         # Iterate through each table
         for table in table_containers:
             # Find the header and parse it
-            header = table.findAll('tr', attrs={'class': 'data-table__headers'})
-            if len(header) != 1:
-                logger.Logger.log(logger.LogLevel.WARN, 'Found %d headers, more than 1' % (len(header)))
+            header_cell_titles = BloombergParser.parse_header_titles_from_table(table)
+            if header_cell_titles is None:
                 continue
 
-            header_cells = header[0].findAll('th', attrs={'class': 'data-table__headers__cell'})
-            # A list of titles of the table like ['INDEX', 'UNITS', 'PRICE', 'CHANGE', '%CHANGE']
-            header_cell_titles = [cell.get_text() for cell in header_cells]
-
             # Parse all the data rows from the current table
-            data_rows = table.findAll('tr', attrs={'class': 'data-table__row'})
+            data_rows = BloombergParser.parse_rows_from_table(table)
             for row in data_rows:
-                row_cells = row.findAll('td', attrs={'class': 'data-table__row__cell'})
+                row_cells = BloombergParser.parse_values_from_row(row)
                 if (len(row_cells) != len(header_cell_titles)):
                     logger.Logger.log(
                             logger.LogLevel.WARN,
@@ -52,13 +72,10 @@ class BloombergParser:
                                     len(row_cells), len(header_cell_titles)))
                     continue
 
-                # A list of values like ['CL1:COM', 'USD/bll.', '39.44', '-1.89%']
-                row_cell_values = [BloombergParser.clean_name(cell.get_text()) for cell in row_cells]
                 # Zip the titles and values together to create a list of tuple (title, value)
-                titles_and_values = zip(header_cell_titles, row_cell_values)
+                titles_and_values = zip(header_cell_titles, row_cells)
                 logger.Logger.log(logger.LogLevel.INFO, 'Found following values in the cell: %s' % titles_and_values)
 
-
 if __name__ == '__main__':
-    bloomberg = BloombergParser();
-    bloomberg.parse('http://www.bloomberg.com/energy')
+    parser = BloombergParser()
+    parser.parse('http://www.bloomberg.com/markets/commodities')
--- QueryService/bloomberg_parser_test.py
+++ QueryService/bloomberg_parser_test.py
@@ -8,11 +8,33 @@ import bloomberg_parser
 
 
 class TestBloombergParser(unittest.TestCase):
-    def test_get_table_containers(self):
+    def test_parse(self):
         with open(".\\QueryService\\test_html\\bloomberg_commodities.html", "r") as file:
             html = BeautifulSoup(file.read(), 'html.parser')
-            table_containers = bloomberg_parser.BloombergParser.parse_table_containers(html)
+            table_containers = bloomberg_parser.BloombergParser.parse_table_containers_from_html(html)
             self.assertEqual(4, len(table_containers))
+
+            for index in range(len(table_containers)):
+                table = table_containers[index]
+                # Find the header and parse it to get header titles and verify them
+                header_cell_titles = bloomberg_parser.BloombergParser.parse_header_titles_from_table(table)
+                if index == 0:
+                    expected_titles = ['index', 'value', 'change', '%change', 'high', 'low', 'time (edt)', '2 day']
+                    self.assertEqual(len(expected_titles), len(header_cell_titles))
+                    for index2 in range(len(expected_titles)):
+                        self.assertEqual(expected_titles[index2], header_cell_titles[index2])
+                else:
+                    expected_titles = ['index', 'units', 'price', 'change', '%change', 'contract', 'time (edt)', '2 day']
+                    self.assertEqual(len(expected_titles), len(header_cell_titles))
+                    for index2 in range(len(expected_titles)):
+                        self.assertEqual(expected_titles[index2], header_cell_titles[index2])
+
+                # Parse all the data rows from the current table and verify them
+                data_rows = bloomberg_parser.BloombergParser.parse_rows_from_table(table)
+                self.assertEqual(5, len(data_rows))
+                for row in data_rows:
+                    row_cells = bloomberg_parser.BloombergParser.parse_values_from_row(row)
+                    self.assertEqual(8, len(row_cells))
 
 
 if __name__ == '__main__':
