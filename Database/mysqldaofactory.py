bf7eb0cd51c19b115e5d0d64d63e9d92a55281f4
hungtantran <hungtantran@live.com>
2016-03-23 23:52:04 -0700

    Add create table test mysql

--- Database/mysqldaofactory.py
+++ Database/mysqldaofactory.py
@@ -7,4 +7,18 @@ from daofactory import DAOFactory
 class MysqlDAOFactory(DAOFactory):
     @staticmethod
     def create(username, password, server, database):
-        return MySQLdb.connect(server, username, password, database)
+        return MysqlConnection(MySQLdb.connect(server, username, password, database))
+
+
+class MysqlConnection(MySQLdb.connection):
+    def __init__(self, connection):
+        self.connection = connection
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+    def cursor(self):
+        return self.connection.cursor()
--- Database/mysqldaofactory_test.py
+++ Database/mysqldaofactory_test.py
@@ -7,17 +7,38 @@ from daofactoryrepo import DAOFactoryRepository
 
 
 class TestMysqlDAOFactory(unittest.TestCase):
-    def test_createdb(self):
+    username = 'root'
+    password = 'test'
+    server = '104.154.40.63'
+    database = 'models'
+
+    def test_connection(self):
         mysql_dao_factory = DAOFactoryRepository.getInstance('mysql')
-        mysql_connection = mysql_dao_factory.create('root', 'amazonebay1', '104.154.40.63', 'models')
-        mysql_cursor = mysql_connection.cursor()
-        mysql_cursor.execute("SELECT VERSION()")
-        data = mysql_cursor.fetchone()
+        with mysql_dao_factory.create(TestMysqlDAOFactory.username,
+                                      TestMysqlDAOFactory.password,
+                                      TestMysqlDAOFactory.server,
+                                      TestMysqlDAOFactory.database) as mysql_connection:
+            mysql_cursor = mysql_connection.cursor()
+            mysql_cursor.execute("SELECT VERSION()")
+            data = mysql_cursor.fetchone()
 
-        logger.Logger.log(logger.LogLevel.INFO, 'Mysql version %s' % data)
-        self.assertGreater(len(data), 0)
-        mysql_connection.close()
+            logger.Logger.log(logger.LogLevel.INFO, 'Mysql version %s' % data)
+            self.assertGreater(len(data), 0)
 
+    def test_createtable(self):
+        mysql_dao_factory = DAOFactoryRepository.getInstance('mysql')
+        with mysql_dao_factory.create(TestMysqlDAOFactory.username,
+                                      TestMysqlDAOFactory.password,
+                                      TestMysqlDAOFactory.server,
+                                      TestMysqlDAOFactory.database) as mysql_connection:
+            mysql_cursor = mysql_connection.cursor()
+            mysql_cursor.execute("DROP TABLE IF EXISTS TEST1")
+            mysql_cursor.execute("CREATE TABLE TEST1 (FIRST_NAME  CHAR(20) NOT NULL)")
+            mysql_cursor.execute("SHOW TABLES LIKE 'TEST1'")
+
+            table = mysql_cursor.fetchone()
+            self.assertEqual(1, len(table))
+            self.assertEqual("TEST1", table[0])
 
 if __name__ == '__main__':
     unittest.main()
