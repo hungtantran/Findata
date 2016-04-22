4f586bf00871c23c70e0989bfd4e1d2e2d088844
Model3655 <nwickham14@outlook.com>
2016-04-22 12:13:15 -0700

    Fix source map errors by fixing dependency version on Babel

--- webserver/rollup.config.js
+++ webserver/rollup.config.js
@@ -3,14 +3,31 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 
+var useSourceMaps = true;
+
 export default {
   entry: 'static/js/main.js',
-  sourceMap: true,
+  sourceMap: useSourceMaps,
   plugins: [
-    babel(),
-    nodeResolve(),
-    commonjs(),
-    replace({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)})
+    babel({
+        exclude: 'node_modules/**',
+        sourceMaps: useSourceMaps,
+        sourceMap: useSourceMaps
+    }),
+    nodeResolve({
+        jsnext: true,
+        main: true
+    }),
+    commonjs({
+        include: 'node_modules/**',
+        sourceMap: useSourceMaps
+    }),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        sourceMap: useSourceMaps
+    })
     ],
-  dest: 'static/bundle.js'
+  dest: 'static/bundle.js',
+  format: 'iife',
+  exports: 'none'
 };
