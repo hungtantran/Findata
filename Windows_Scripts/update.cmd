ffb428dbe01b2c6996a2706d95830a8d14c69885
Hung Tran <Hung Tran>
2016-07-12 23:58:06 -0700

    Add update script for Window to run all update pipeline

---
+++ Windows_Scripts/update.cmd
@@ -0,0 +1,5 @@
+START /B python UpdatePipeline/update_bureau_labor_statistics.py
+START /B python UpdatePipeline/update_exchange_etf.py
+START /B python UpdatePipeline/update_exchange_index.py
+START /B python UpdatePipeline/update_exchange_stockprice.py
+START /B python UpdatePipeline/update_fixedincome_ustreasurybill.py
