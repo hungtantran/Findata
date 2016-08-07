696209512261cfc598c476709dfe0cdf5b8a59f9
Hung Tran <Hung Tran>
2016-08-07 03:15:38 -0700

    Add bootstrap and header

--- webserver/js/components/header.js
+++ webserver/js/components/header.js
@@ -4,7 +4,26 @@ var Header = React.createClass({
   render: function() {
     return (
       <div className="header">
-        Header
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">Findata</a>
+            </div>
+            <div id="navbar" className="collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
