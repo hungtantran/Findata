8d9b73656a3de81cad066f502903867cef02a4c7
nwickham <nwickham@alumni.nd.edu>
2016-09-10 19:44:12 -0700

    Add pan and zoom

--- webserver/js/components/graph/plot.js
+++ webserver/js/components/graph/plot.js
@@ -5,6 +5,8 @@ import XAxis from './xaxis';
 import YAxis from './yaxis';
 import {scaleLinear, scaleTime, scaleOrdinal, schemeCategory10} from 'd3-scale';
 import {max, min, bisector} from 'd3-array';
+import {zoom} from 'd3-zoom';
+import {select, event} from 'd3-selection';
 import Legend from '../legend/legend.js';
 import Entries from 'object.entries';
 
@@ -51,13 +53,22 @@ function getYScaleTranslation(width) {
 class Plot extends React.Component {
     constructor(props) {
         super(props);
+        this.updateZoom = this.updateZoom.bind(this);
         this.buildScale = this.buildScales.bind(this);
         this.buildItems = this.buildItems.bind(this);
         this.onMouseMove = this.onMouseMove.bind(this);
         this.onMouseLeave = this.onMouseLeave.bind(this);
+        this.setInitialState = this.setInitialState.bind(this);
 
         this.buildScales();
-        this.buildItems();
+        this.zoomGenerator = zoom().extent([[0, 0], [this.props.width * .95, this.props.height]]).translateExtent([[0, 0], [this.props.width * .95, this.props.height]]).scaleExtent([1, Infinity]).on('zoom', this.updateZoom);
+        this.setInitialState();
+    }
+
+    updateZoom() {
+        console.log(event.transform);
+        //select(this.refs.items).attr('transform', 'translate(' + event.transform.x + ', 0) scale(' + event.transform.k + ', 1)');
+        this.setState({transformedXScale : event.transform.rescaleX(this.xscale)});
     }
 
     buildScales() {
@@ -67,29 +78,32 @@ class Plot extends React.Component {
         this.yscale = scaleLinear()
             .domain(getDataYDomain(this.props.dataSets))
             .range([this.props.height, 0]);
-        this.colorscale = scaleOrdinal(schemeCategory10);
+        this.colorscale = scaleOrdinal(schemeCategory10);        
+    }
+
+    setInitialState() {
+        this.state = {
+            hoverLines: [],
+            hoverValues: [],
+            transformedXScale: this.xscale
+        };
     }
 
     buildItems() {
-        this.state = {
-            items:
-            Entries(this.props.dataSets).map((pair) => {
-                if(pair[1].type === 'line')
-                    return <Line xscale={this.xscale} yscale={this.yscale} colorscale={this.colorscale} dataSet={pair[1].data} key={pair[0]} colorid={pair[0]} />;
-                else if(pair[1].type === 'bar')
-                    return <Bar xscale={this.xscale} yscale={this.yscale} colorscale={this.colorscale} dataSet={pair[1].data} key={pair[0]} colorid={pair[0]} />;
-            }),
-            hoverLines:[],
-            hoverValues:[]
-        };
+        return Entries(this.props.dataSets).map((pair) => {
+            if(pair[1].type === 'line')
+                return <Line xscale={this.state.transformedXScale} yscale={this.yscale} colorscale={this.colorscale} dataSet={pair[1].data} key={pair[0]} colorid={pair[0]} />;
+            else if(pair[1].type === 'bar')
+                return <Bar xscale={this.state.transformedXScale} yscale={this.yscale} colorscale={this.colorscale} dataSet={pair[1].data} key={pair[0]} colorid={pair[0]} />;
+        });
     }
 
     onMouseMove(event) {
         var elX = event.pageX - this.refs.element.parentNode.getBoundingClientRect().left - this.props.xOffset - window.pageXOffset;
-        var xVal = this.xscale.invert(elX);
-        if(valueIsInDomain(xVal, this.xscale.domain())) {
+        var xVal = this.state.transformedXScale.invert(elX);
+        if(valueIsInDomain(xVal, this.state.transformedXScale.domain())) {
             var style = {strokeWidth: '1', strokeLinecap: 'round', strokeDasharray: '5,5'};
-            var lines = [<Line xscale={this.xscale} yscale={this.yscale} colorscale={this.colorscale} dataSet={[{t: xVal, v: 0}, {t: xVal, v: this.yscale.domain()[1]}]} key={'hoverLinex'} colorid={'0'} style={style} />];
+            var lines = [<Line xscale={this.state.transformedXScale} yscale={this.yscale} colorscale={this.colorscale} dataSet={[{t: xVal, v: 0}, {t: xVal, v: this.yscale.domain()[1]}]} key={'hoverLinex'} colorid={'0'} style={style} />];
             var dateBisector = bisector(function(d) {
                 return new Date(d.t);
             }).right;
@@ -99,12 +113,12 @@ class Plot extends React.Component {
             });
 
             yVal.forEach((val, index) => {
-                lines.push(<Line xscale={this.xscale} yscale={this.yscale} colorscale={this.colorscale} dataSet={[{t: this.xscale.domain()[0], v: val}, {t: this.xscale.domain()[1], v: val}]} key={`hoverLiney${index}`} colorid={'0'} style={style} />);
+                lines.push(<Line xscale={this.state.transformedXScale} yscale={this.yscale} colorscale={this.colorscale} dataSet={[{t: this.state.transformedXScale.domain()[0], v: val}, {t: this.state.transformedXScale.domain()[1], v: val}]} key={`hoverLiney${index}`} colorid={'0'} style={style} />);
             });
 
             var values = [<text x={elX} y={this.yscale.range()[0]} fontFamily="sans-serif" fontSize="15px" fill="red" key={'hovervaluex'}>{xVal.toDateString()}</text>];
             yVal.forEach((val, index) => {
-                values.push(<text x={this.xscale.range()[1]} y={this.yscale(val)} fontFamily="sans-serif" fontSize="15px" fill="red" key={`hovervaluey${index}`}>{val}</text>);
+                values.push(<text x={this.state.transformedXScale.range()[1]} y={this.yscale(val)} fontFamily="sans-serif" fontSize="15px" fill="red" key={`hovervaluey${index}`}>{val}</text>);
             });
 
             this.setState({
@@ -131,17 +145,23 @@ class Plot extends React.Component {
     componentDidMount() {
         this.refs.element.parentNode.addEventListener('mousemove', this.onMouseMove);
         this.refs.element.parentNode.addEventListener('mouseleave', this.onMouseLeave);
+        select(this.refs.zoom).call(this.zoomGenerator);
     }
 
     render() {
+        var items = this.buildItems();
+
         return (
             <g className="plot" transform={getPlotTranslation(this.props.xOffset, this.props.yOffset)} ref="element" >
-                {this.state.items}
+                <g ref="items" >
+                    {items}
+                    <XAxis key="axis" scale={this.state.transformedXScale} translate={getXScaleTranslation(this.props.height)} />
+                </g>
                 {this.state.hoverLines}
-                {this.state.hoverValues}                
-                <XAxis key="axis" scale={this.xscale} translate={getXScaleTranslation(this.props.height)} />
+                {this.state.hoverValues}
                 <YAxis key="yaxis" scale={this.yscale} translate={getYScaleTranslation(this.props.width * .95)} />
                 <Legend xpos={this.props.width * .95} ypos={0} colorscale={this.colorscale} items={this.props.dataSets} />
+                <rect width={this.props.width * .95} height={this.props.height} fill="none" pointerEvents="all" ref="zoom" />
             </g>
         );
     }
