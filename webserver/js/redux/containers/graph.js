f4d3748dede6e9c04272f4a640815fda78cf1d0c
Nate Wickham <nwickham@wdbmbp-loaner-5.local>
2016-11-24 21:20:46 -0800

    Fix bug where legend didn't update when you added a data set to a plot

--- webserver/js/redux/containers/graph.js
+++ webserver/js/redux/containers/graph.js
@@ -30,6 +30,9 @@ class Graph extends React.Component {
         if(this.props.plots.length != nextProps.plots.length)
             return true;
 
+        if(this.props.items.length != nextProps.items.length)
+            return true;
+
         if(this.props.width != nextProps.width || this.props.height != nextProps.height)
             return true;
         
